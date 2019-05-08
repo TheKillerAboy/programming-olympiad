6cc2ce0fd6dacefd412fdd1d6f2e09a2260678f3
TheKillerAboy <36484821+TheKillerAboy@users.noreply.github.com>
2019-05-08 19:45:23 +0200

    Added Final Round 2011 Day 1 Problem 1 Finding Squares Solution 1

---
+++ Python Solutions/Final Round 2011/Problem 1 Finding Squares Solution 1.py
@@ -0,0 +1,63 @@
+from itertools import combinations
+
+class Coor:
+    def __init__(self, location):
+        self.x, self.y = location
+
+    @classmethod
+    def xy(self, x, y):
+        return Coor((x, y))
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+    def __sub__(self, other):
+        x, y = tuple(other)
+        return Coor((self.x-x,self.y-y))
+
+    def __add__(self, other):
+        x,y = tuple(other)
+        return Coor((self.x+x,self.y+y))
+
+    def length_easy(self):
+        return self.x**2+self.y**2
+
+    def __len__(self):
+        return self.length_easy()**0.5
+
+    def __hash__(self):
+        return hash(tuple(self))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __repr__(self):
+        return f'x: {self.x} y: {self.y}'
+
+def isSquare(points):
+    diff = set()
+    for i1,p1 in enumerate(points):
+        for p2 in points[i1+1:]:
+            diff.add((p1-p2).length_easy())
+    if len(diff) == 2:
+        return True
+    return False
+
+def isAlign(points):
+    x_points = {point.x for point in points}
+    y_points = {point.y for point in points}
+    if len(x_points)==2 and len(y_points)==2:
+        return True
+    return False
+
+if __name__ == '__main__':
+    amount = int(input(''))
+    points = []
+    for _ in range(amount):
+        points.append(Coor((int(_) for _ in input('').split(' '))))
+    squares = 0
+    for pos_square in combinations(points,4):
+        if isAlign(pos_square) and isSquare(pos_square):
+            squares+=1
+    print(squares)
