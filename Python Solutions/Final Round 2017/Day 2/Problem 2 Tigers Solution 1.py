1feb97a25fb33c8b2f40cf81955234fdec3671bb
TheKillerAboy <36484821+TheKillerAboy@users.noreply.github.com>
2019-07-10 17:15:14 +0200

    Update Problem 2 Tigers Solution 1.py

--- Python Solutions/Final Round 2017/Day 2/Problem 2 Tigers Solution 1.py
+++ Python Solutions/Final Round 2017/Day 2/Problem 2 Tigers Solution 1.py
@@ -1,41 +1,44 @@
-MOD = 10**9+7
+MOD = 10 ** 9 + 7
 
-def get_locs(start,distance,direction):
-    for i in range(1,distance+1):
-        loc = start+direction*i
+
+def get_locs(start, distance, direction):
+    for i in range(1, distance + 1):
+        loc = start + direction * i
         if loc in hills.keys():
             yield loc
-        if loc == mi or loc ==ma:
+        if loc == mi or loc == ma:
             yield loc
 
 
-def poss(start,direction):
-    locs = list(get_locs(start,L,direction))
+def poss(start, direction):
+    locs = list(get_locs(start, L, direction))
     if len(locs) == 0:
         return 1
     else:
         out = len(locs)
         for loc in locs:
-            out *= poss(loc,direction)
+            out *= poss(loc, direction)
             out %= MOD
         return out
 
+
 def get_inputs():
-    return map(int,input('').split(' '))
+    return map(int, input('').split(' '))
+
 
 if __name__ == '__main__':
-    N,L = get_inputs()
+    N, L = get_inputs()
     hills = {}
-    top = (0,0)
-    mi,ma = 9999999999,0
+    top = (0, 0)
+    mi, ma = 9999999999, 0
     for _ in range(N):
-        l,h = get_inputs()
-        mi = min(mi,l)
-        ma = max(ma,l)
+        l, h = get_inputs()
+        mi = min(mi, l)
+        ma = max(ma, l)
         hills[l] = h
         if h > top[1]:
-            top = l , h
+            top = l, h
     mi -= 1
     ma += 1
     top = top[0]
-    print((poss(top,-1)+poss(top,1))%MOD)
+    print((poss(top, -1) + poss(top, 1)) % MOD)
