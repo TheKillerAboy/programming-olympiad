8f7ea1f570d5c9f019c34c6ea74c154ee88dde40
TheKillerAboy <aboy001@outlook.com>
2021-03-29 21:50:55 +0200

    Finally completed SAPO/SR_2015/5 on both python and c++

--- Python_Solutions/SAPO/School_Round_2015/Problem_5_Prime_Generating_Integers_Solution_3.py
+++ Python_Solutions/SAPO/School_Round_2015/Problem_5_Prime_Generating_Integers_Solution_3.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 def gen_primes(n):
     primes = [True]*(n+1)
@@ -11,15 +12,23 @@ def gen_primes(n):
 
 
 def is_proper(n):
-    out = True
-    for i in range(2,math.floor(math.sqrt(n))+1):
+    for i in range(1,math.floor(math.sqrt(n))+1):
         if n%i == 0:
             if (i+n//i) not in primes:
-                out = False
-    return out
+                return False
+    return True
+
 
 inp = int(input())
-primes = list(gen_primes(inp+1))
+start = time.time()
+
+def delta():
+    global start
+    out = time.time()-start
+    start = time.time()
+    return out
+primes = set(gen_primes(inp+1))
+print('Got primes', delta())
 
 pos = [False]*(inp+1)
 pos[1] = True
@@ -29,9 +38,12 @@ for i in range(2,inp+1,4):
 for prime in primes:
     for j in range(2*prime*prime,inp+1,prime*prime):
         pos[j] = False
+print('Initial check', delta())
 
 out = 0
 for i in range(inp+1):
+    if i%1000000==0:
+        print(f'Final Check: {i}', delta())
     if pos[i] and is_proper(i):
         out+=i
 print(out)
